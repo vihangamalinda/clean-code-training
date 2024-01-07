14df7338414d1770124fd7ea45f74698b8d15f75
Vihanga Malinda <vihangamalindafernando@gmail.com>
2024-01-07 00:49:57 -0800

    test:Refactoring testcase of Aged Brie.

--- src/test/java/com/a/introduction/gildedrose/MyTestGlidedRoseBAgedBrieTest.java
+++ src/test/java/com/a/introduction/gildedrose/MyTestGlidedRoseBAgedBrieTest.java
@@ -9,18 +9,45 @@ public class MyTestGlidedRoseBAgedBrieTest {
 
     public static final int AGED_BRIE_SELL_IN = 4;
     public static final int AGED_BRIE_QUALITY = 3;
+    public static final int CURRENT_SELL_IN = -1;
+    public static final int CURRENT_AGED_BRIE_QUALITY = 3;
+    public static final int ULTIMATE_QUALITY_AGED_BRIE_SELL_IN = 4;
+    public static final int ULTIMATE_AGED_BRIE_QUALITY = 50;
     private final String AGED_BRIE_ITEM = "Aged Brie";
 
     @Test
     public void testUpdateQuality_WhenAgedBrie_thenSellInDecreasedByOneAndQualityIncreasedByOne() {
 //        Arrange
-        Item item = createItem(AGED_BRIE_ITEM, AGED_BRIE_SELL_IN, AGED_BRIE_QUALITY);
-        Item[] items = new Item[]{item};
+        Item[] items = new Item[]{createItem(AGED_BRIE_ITEM, AGED_BRIE_SELL_IN, AGED_BRIE_QUALITY)};
         GildedRose app = createGildedRose(items);
 //        Act
         app.updateQuality();
 //        Assert
         Item expected = createItem(AGED_BRIE_ITEM, AGED_BRIE_SELL_IN - 1, AGED_BRIE_QUALITY + 1);
+        assertItem(expected, app.items[0]);
+    }
+
+    @Test
+    public void testUpdateQuality_WhenAgedBrieWithNegativeOneSellIn_thenSellInDecreasedByOneAndQualityIncreasedByTwo() {
+//        Arrange
+        Item[] items = new Item[]{createItem(AGED_BRIE_ITEM, CURRENT_SELL_IN, CURRENT_AGED_BRIE_QUALITY)};
+        GildedRose app = new GildedRose(items);
+//        Act
+        app.updateQuality();
+//        Assert
+        Item expected = createItem(AGED_BRIE_ITEM, CURRENT_SELL_IN - 1, CURRENT_AGED_BRIE_QUALITY + 2);
+        assertItem(expected, app.items[0]);
+    }
+
+    @Test
+    public void testUpdateQuality_WhenAgedBrieWithMaximumQuality_thenSellInDecreasedByOneAndQualityStaySame() {
+//        Arrange
+        Item[] items = new Item[]{createItem(AGED_BRIE_ITEM, ULTIMATE_QUALITY_AGED_BRIE_SELL_IN, ULTIMATE_AGED_BRIE_QUALITY)};
+        GildedRose app = createGildedRose(items);
+//        Act
+        app.updateQuality();
+//        Assert
+        Item expected = createItem(AGED_BRIE_ITEM, ULTIMATE_QUALITY_AGED_BRIE_SELL_IN - 1, ULTIMATE_AGED_BRIE_QUALITY);
         assertItem(expected, app.items[0]);
     }
 
