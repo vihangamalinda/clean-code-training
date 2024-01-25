562fd90d1d9fb23c0f81c23712c1e4d19033915f
Vihanga Malinda <vihangamalindafernando@gmail.com>
2024-01-25 08:58:38 -0800

    refactor:Refactoring code.

--- src/main/java/com/b/simple/design/business/student/StudentHelper.java
+++ src/main/java/com/b/simple/design/business/student/StudentHelper.java
@@ -1,12 +1,13 @@
 package com.b.simple.design.business.student;
 
 import static com.b.simple.design.business.student.Grade.*;
+import static com.b.simple.design.business.student.Qualifier.*;
 
 public class StudentHelper {
 
-    public static final int GRADE_B_UPPER_LIMIT = 80;
-    public static final int GRADE_B_LOWER_LIMIT = 51;
-    public static final int EXTRA_UPPER_LIMIT_FOR_MATH = 10;
+    private static final int GRADE_B_UPPER_LIMIT = 80;
+    private static final int GRADE_B_LOWER_LIMIT = 51;
+    private static final int EXTRA_UPPER_LIMIT_FOR_MATH = 10;
 
 
     /* PROBLEM 1 */
@@ -38,6 +39,11 @@ public class StudentHelper {
         return GRADE_C.toString();
     }
 
+    //   Question 3 constant
+    private static final int UNQUALIFIED_UPPER_LIMIT = 20;
+    private static final int QUALIFIED_UPPER_LIMIT = 80;
+    private static final int EXTRA_LIMIT_FOR_MATH_Q3 = 5;
+
     /*  PROBLEM 3
      * You and your Friend are planning to enter a Subject Quiz.
      * However, there is a marks requirement that you should attain to qualify.
@@ -55,9 +61,12 @@ public class StudentHelper {
      */
 
     public String willQualifyForQuiz(int marks1, int marks2, boolean isMaths) {
-        if ((isMaths ? marks1 <= 25 : marks1 <= 20) || (isMaths ? marks2 <= 25 : marks2 <= 20)) return "NO";
-        if ((isMaths ? marks1 >= 85 : marks1 >= 80) || (isMaths ? marks2 >= 85 : marks2 >= 80)) return "YES";
-        return "MAYBE";
+        int extraLimit = isMaths ? EXTRA_LIMIT_FOR_MATH_Q3 : 0;
+        if ((marks1 <= UNQUALIFIED_UPPER_LIMIT + extraLimit) || (marks2 <= UNQUALIFIED_UPPER_LIMIT + extraLimit))
+            return NO.toString();
+        if ((marks1 >= QUALIFIED_UPPER_LIMIT + extraLimit) || (marks2 >= QUALIFIED_UPPER_LIMIT + extraLimit))
+            return YES.toString();
+        return MAYBE.toString();
     }
 
 
@@ -72,6 +81,24 @@ enum Grade {
     private final String label;
 
     Grade(String label) {
+        this.label = label;
+    }
+
+    @Override
+    public String toString() {
+        return this.label;
+    }
+}
+
+//Qualifier enum
+enum Qualifier {
+    YES("YES"),
+    MAYBE("MAYBE"),
+    NO("NO");
+
+    private final String label;
+
+    Qualifier(String label) {
         this.label = label;
     }
 
