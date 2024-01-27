8e922e56922d437cf2c04cfc17253b52a001c428
Vihanga Malinda <vihangamalindafernando@gmail.com>
2024-01-27 00:36:34 -0800

    fix:Improving the readerbility of the code.

--- src/main/java/com/b/simple/design/business/text/TextHelper.java
+++ src/main/java/com/b/simple/design/business/text/TextHelper.java
@@ -2,18 +2,18 @@ package com.b.simple.design.business.text;
 
 public class TextHelper {
 
-	public String swapLastTwoCharacters(String str) {
-		if (str==null)return "";
-		int stringLength = str.length();
-		if (stringLength <= 1) {
-			return str;
-		}
-		char lastChar = str.charAt(stringLength - 1);
-		char oneBeforeLastChar = str.charAt(stringLength - 2);
-		return str.substring(0, stringLength - 2) + lastChar + oneBeforeLastChar;
-	}
+    public String swapLastTwoCharacters(String str) {
+        if (str == null) return "";
+        int stringLength = str.length();
+        if (stringLength <= 1) {
+            return str;
+        }
+        int lastIndex = stringLength - 1;
+        int oneBeforeLastIndex = stringLength - 2;
+        return str.substring(0, oneBeforeLastIndex) + str.charAt(lastIndex) + str.charAt(oneBeforeLastIndex);
+    }
 
-	public String truncateAInFirst2Positions(String str) {
-		return null;
-	}
+    public String truncateAInFirst2Positions(String str) {
+        return null;
+    }
 }
