d9e559025667d50babbf64b939563a10af12c251
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2024-02-10 11:40:09 -0800

    feat:Adding the method to pass the test cases with TDD.

--- src/main/java/com/b/simple/design/business/text/TextHelper.java
+++ src/main/java/com/b/simple/design/business/text/TextHelper.java
@@ -1,6 +1,14 @@
 package com.b.simple.design.business.text;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class TextHelper {
+
+    private static final String STRING_A = "A";
 
     public String swapLastTwoCharacters(String str) {
         if (str == null) return "";
@@ -15,6 +23,23 @@ public class TextHelper {
     }
 
     public String truncateAInFirst2Positions(String str) {
-        return null;
+        if (str == null || str.isEmpty()) return "";
+        if (isCharAContainsOnFirst2Positions(str)) {
+            return getFirstTwoCharactersValidated(str) + str.substring(2);
+        }
+        return str;
+    }
+
+    private static String getFirstTwoCharactersValidated(String str) {
+        String[] firstTwoCharacters = str.substring(0, 2).split("");
+        return Stream.of(firstTwoCharacters).map(character -> isCharacterA(character) ? "" : character).collect(Collectors.joining());
+    }
+
+    private static boolean isCharacterA(String character) {
+        return character.equals(STRING_A);
+    }
+
+    private static boolean isCharAContainsOnFirst2Positions(String str) {
+        return str.substring(0, 2).contains(STRING_A);
     }
 }
